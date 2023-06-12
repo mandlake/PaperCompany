27fafd98c265f8953edad353a69b7602494d120d
SunHyeYoon <haehae96@jbnu.ac.kr>
2023-06-12 21:34:19 +0900

    Revert "map"
    This reverts commit 41559052ece34f0c1687f5ed96e3b7ec107331f8.

--- PaperCompany/Assets/Script/HitBox.cs
+++ PaperCompany/Assets/Script/HitBox.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class HitBox : MonoBehaviour, IDamageable
 {
     public float hp = 100;  // 에너미 체력
-    public GameObject GM;
+    public int enemyScore;   // 에너미 점수
 
     public void OnDamage(float damageAmount, int score)
     {
         hp -= damageAmount;
+        enemyScore += score;
         if (hp <= 0)
         {
             gameObject.SetActive(false);
-            GM.GetComponent<GameManager>().GetScore(score);
         }
     }
 }
--- PaperCompany/Assets/Script/StartMenu.cs
+++ PaperCompany/Assets/Script/StartMenu.cs
@@ -8,10 +8,17 @@ public class StartMenu : MonoBehaviour
     [Header("UI Pages")]
     public GameObject title;
     public GameObject menu;
+    public GameObject settings;
+    public GameObject credit;
+    public GameObject newLoad;
 
     [Header("Main Menu Buttons")]
     public Button startButton;
+    public Button settingsButton;
+    public Button creditButton;
     public Button quitButton;
+    public Button newButton;
+    public Button loadButton;
 
     public List<Button> returnButtons;
 
@@ -20,8 +27,12 @@ public class StartMenu : MonoBehaviour
         EnableMainMenu();
 
         //Hook events
-        startButton.onClick.AddListener(StartGame);
+        startButton.onClick.AddListener(EnableNewLoad);
+        settingsButton.onClick.AddListener(EnableSettings);
+        creditButton.onClick.AddListener(EnableCredit);
         quitButton.onClick.AddListener(QuitGame);
+        newButton.onClick.AddListener(NewGame);
+        loadButton.onClick.AddListener(LoadGame);
 
         foreach (var item in returnButtons)
         {
@@ -34,22 +45,60 @@ public class StartMenu : MonoBehaviour
         Application.Quit();
     }
 
+    public void EnableNewLoad()
+    {
+        title.SetActive(true);
+        menu.SetActive(false);
+        settings.SetActive(false);
+        credit.SetActive(false);
+        newLoad.SetActive(true);
+    }
+
     // 새 게임
-    public void StartGame()
+    public void NewGame()
     {
         HideAll();
-        SceneTransitionManager.singleton.GoToSceneAsync(1);
+        //SceneTransitionManager.singleton.GoToSceneAsync(1);   // 씬 전환
+    }
+
+    // 불러오기
+    public void LoadGame()
+    {
+        HideAll();
+        //SceneTransitionManager.singleton.GoToSceneAsync(1);   // 씬 전환
     }
 
     public void HideAll()
     {
         title.SetActive(false);
         menu.SetActive(false);
+        settings.SetActive(false);
+        credit.SetActive(false);
+        newLoad.SetActive(false);
     }
 
     public void EnableMainMenu()
     {
         title.SetActive(true);
         menu.SetActive(true);
+        settings.SetActive(false);
+        credit.SetActive(false);
+        newLoad.SetActive(false);
+    }
+    public void EnableSettings()
+    {
+        title.SetActive(false);
+        menu.SetActive(false);
+        settings.SetActive(true);
+        credit.SetActive(false);
+        newLoad.SetActive(false);
+    }
+    public void EnableCredit()
+    {
+        title.SetActive(false);
+        menu.SetActive(false);
+        settings.SetActive(false);
+        credit.SetActive(true);
+        newLoad.SetActive(false);
     }
 }
